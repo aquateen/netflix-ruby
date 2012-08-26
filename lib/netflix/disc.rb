6086019376dc6c698ae56a56d4e7118b42161789
Chris Johnson <chris@mbp-3.local>
2012-08-26 03:41:58 -0700

    Make other disc attributes accessible.

--- lib/netflix/disc.rb
+++ lib/netflix/disc.rb
@@ -2,10 +2,14 @@ require 'netflix/json_resource'
 
 module Netflix
   class Disc < JsonResource
-    define_getter :id, :updated
+    define_getter :id, :updated, :release_year, :runtime
     
     def title
       @map["title"]["regular"]
+    end
+    
+    def box_art
+      @map["box_art"]["large"]
     end
   end
 end
